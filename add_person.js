b8d28eeccae8a6b7d145a30e89fb585a4148ce39
Mariana <mariana.horvat.qa@gmail.com>
2019-05-03 02:14:09 +0000

    Using Knex to perform an insert

---
+++ add_person.js
@@ -0,0 +1,58 @@
+const pg = require("pg");
+const settings = require("./settings"); // settings.json
+
+const client = new pg.Client({
+  user     : settings.user,
+  password : settings.password,
+  database : settings.database,
+  host     : settings.hostname,
+  port     : settings.port,
+  ssl      : settings.ssl
+});
+
+let inputName = process.argv[2];
+
+const query = {
+  text: 'SELECT * FROM famous_people WHERE first_name= $1::text OR last_name= $1::text;',
+  values: [inputName]   
+}
+
+function lookupPerson (input) {
+  client.query(query, (error,result) => {
+      if (error) {
+          return console.error("error running query", error);
+      }
+
+      console.log(`Found ${result.rows.length} person(s) by name ${inputName}:`);
+      
+      result.rows.forEach( (value, i) => { 
+          console.log(`${i+1}. ${value.first_name} ${value.last_name}, born '${value.birthdate.toISOString().slice(0, 10)}'`) 
+      });
+      client.end();
+  });
+}
+//toISOString() method converts a Date object into a string, using the ISO standard
+
+client.connect((err) => {
+  if (err) {
+    return console.error("Connection Error", err);
+  }
+  console.log("Searching in database...");
+  lookupPerson(inputName);
+});
+
+
+knex.select('name').from('users')
+  .where('id', '>', 20)
+  .andWhere('id', '<', 200)
+  .limit(10)
+  .offset(x)
+  .asCallback(function(err, rows) {
+    if (err) return console.error(err);
+    knex.select('id').from('nicknames')
+      .whereIn('nickname', _.pluck(rows, 'name'))
+      .asCallback(function(err, rows) {
+        if (err) return console.error(err);
+        console.log(rows);
+      });
+  });
